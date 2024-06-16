b12cb794d8237bcb081583948ab0f8949d2454a3
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2024-06-16 12:20:45 -0300

    feat: creating a form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { Mail } from "lucide-react"
+
 import { SettingsTabs } from "@/components/SettingsTabs"
+import Input from "@/components/Input"
 
 export default function Home() {
   return (
@@ -6,6 +9,145 @@ export default function Home() {
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
 
       <SettingsTabs />
+
+      <div className="mt-6">
+        <div className="flex items-center justify-between border-b border-zinc-200 pb-5">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900"></h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              form="settings"
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form id="settings" className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200">
+          {/* NAME */}
+          <div className="grid-cols-form grid gap-3">
+            <label htmlFor="firstName" className="text-sm font-medium text-zinc-700">
+              Name
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Control id="fistName" defaultValue="Erikson" />
+              </Input.Root>
+              <Input.Root>
+                <Input.Control defaultValue="Sanchez" />
+              </Input.Root>
+            </div>
+            <div />
+          </div>
+
+          {/* EMAIL */}
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="email" className="text-sm font-medium text-zinc-700">
+              Email address
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Prefix>
+                  <Mail className="h-5 w-5 text-zinc-500" />
+                </Input.Prefix>
+
+                <Input.Control
+                  id="email"
+                  type="email"
+                  defaultValue="eriksongoncalves@yahoo.com.br"
+                />
+              </Input.Root>
+            </div>
+            <div />
+          </div>
+
+          {/* YOUR PHOTO */}
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="photo" className="text-sm font-medium text-zinc-700">
+              Your photo
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                This will be displayed on your profile.
+              </span>
+            </label>
+
+            <div />
+          </div>
+
+          {/* ROLE */}
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+              Role
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Control id="role" defaultValue="Developer" />
+              </Input.Root>
+            </div>
+            <div />
+          </div>
+
+          {/* COUNTRY */}
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="country" className="text-sm font-medium text-zinc-700">
+              Country
+            </label>
+            <div className="grid grid-cols-2 gap-6">SELECT</div>
+            <div />
+          </div>
+
+          {/* BIO */}
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
+              Bio
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                Write a short introduction
+              </span>
+            </label>
+
+            <div />
+          </div>
+
+          {/* PORTIFOLIO */}
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="portifolio" className="text-sm font-medium text-zinc-700">
+              Portifolio projects
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                Share a few snippets of your work
+              </span>
+            </label>
+
+            <div />
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,16 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        app: 'minmax(18rem, 20rem) 1fr'
+        app: "minmax(18rem, 20rem) 1fr",
+        form: "minmax(7.5rem, 17.5rem)  minmax(25rem, 1fr) minmax(0, 15rem)"
       }
     }
   },
