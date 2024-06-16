acca5fe76c92df570b84f24e375fa7ebb95d477a
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2024-06-16 11:08:06 -0300

    feat: input component

---
+++ src/app/components/Input/index.tsx
@@ -0,0 +1,36 @@
+import { ComponentProps, InputHTMLAttributes } from "react"
+
+type InputPrefixProps = {} & ComponentProps<"div">
+
+function InputPrefix({ ...rest }: InputPrefixProps) {
+  return <div {...rest} />
+}
+
+type InputControlProps = {} & InputHTMLAttributes<HTMLInputElement>
+
+function InputControl({ ...rest }: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      placeholder="Search"
+      {...rest}
+    />
+  )
+}
+
+type InputRootProps = {} & ComponentProps<"div">
+
+function InputRoot({ ...rest }: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...rest}
+    />
+  )
+}
+
+export default {
+  Prefix: InputPrefix,
+  Control: InputControl,
+  Root: InputRoot
+}
